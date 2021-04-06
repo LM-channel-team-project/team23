553240b9ef45e4d7001a47c6dd5d129c6ff98ecb
Yongveloper <aaa9015@naver.com>
2021-04-06 14:01:06 +0900

    GlobalSyles(reset) 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyles from './Components/Common/GlobalStyles';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div>App</div>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
