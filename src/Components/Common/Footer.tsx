7e004d4125aafa422c11cc1aa1ae8753ec66f4b8
maru5mango <mjel0507@gmail.com>
2021-04-09 12:53:36 +0900

    footer

--- src/Components/Common/Footer.tsx
+++ src/Components/Common/Footer.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Footer = () => <h2>footer</h2>;
+const Footerstyle = styled.div`
+  background-color: #00112e;
+  padding: 20px 0;
+  z-index: 99;
+  position: relative;
+  color: white;
+  text-align: center;
+  font-size: 0.8rem;
+`;
+
+const Footer = () => (
+  <Footerstyle>
+    <span>©2021 Team_Sunday. All rights Reserved.</span>
+  </Footerstyle>
+);
 
 export default Footer;
