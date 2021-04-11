6a8c9988c78678a6f60d9c9b994636f7af325746
Yongveloper <aaa9015@naver.com>
2021-04-11 21:06:23 +0900

    ProjectBox 컴포넌트 분리

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Title from '../../Components/Common/Title';
 import PersonBoxList from '../../Components/PersonBoxList';
+import ProjectBoxList from '../../Components/Project/ProjectBoxList';
 import ProjectBox from '../../Components/Project/ProjectBox';
 import Info from '../../Components/Home/Info';
 import styled from 'styled-components';
@@ -14,11 +15,23 @@ const Home = () => (
     <Info />
     <div className="new_project">
       <Title subtitle="New Project" title="신규 프로젝트가 나왔어요" />
-      <ProjectBox />
+      <ProjectBoxList>
+        <ProjectBox
+          title="간단한 웹 게임 사이드 프로젝트"
+          description="안녕하세요! 팀원 모집하고 있습니다!! 간단한 웹 게임 서비스를 생각하고 있습니다. 실력 상관 없이 즐겁게 하면 좋겠습니다."
+          state={true}
+        />
+      </ProjectBoxList>
     </div>
     <div className="join_project">
       <Title subtitle="Study With Us" title="모집중인 프로젝트를 둘러봐요" />
-      <ProjectBox />
+      <ProjectBoxList>
+        <ProjectBox
+          title="간단한 웹 게임 사이드 프로젝트"
+          description="안녕하세요! 팀원 모집하고 있습니다!!"
+          state={true}
+        />
+      </ProjectBoxList>
     </div>
     <div className="new_user">
       <Title subtitle="New Co-Worker" title="가입을 축하드려요" />
--- client/src/Pages/Project/Project.tsx
+++ client/src/Pages/Project/Project.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import ProjectHeader from '../../Components/Project/ProjectHeader';
 import ProjectBox from '../../Components/Project/ProjectBox';
+import ProjectBoxList from '../../Components/Project/ProjectBoxList';
 
 const Container = styled.section`
   max-width: 1200px;
@@ -12,7 +13,28 @@ const Container = styled.section`
 const Project = () => (
   <Container>
     <ProjectHeader />
-    <ProjectBox />
+    <ProjectBoxList>
+      <ProjectBox
+        title="간단한 웹 게임 사이드 프로젝트"
+        description="안녕하세요! 팀원 모집하고 있습니다!! 간단한 웹 게임 서비스를 생각하고 있습니다. 실력 상관 없이 즐겁게 하면 좋겠습니다."
+        state={true}
+      />
+      <ProjectBox
+        title="간단한 웹 게임 사이드 프로젝트"
+        description="안녕하세요! 팀원 모집하고 있습니다!!"
+        state={true}
+      />
+      <ProjectBox
+        title="간단한 웹 게임 사이드 프로젝트"
+        description="안녕하세요! 팀원 모집하고 있습니다!!"
+        state={true}
+      />
+      <ProjectBox
+        title="간단한 웹 게임 사이드 프로젝트"
+        description="안녕하세요! 팀원 모집하고 있습니다!!"
+        state={true}
+      />
+    </ProjectBoxList>
   </Container>
 );
 
