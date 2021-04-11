e1b67e143bdfabb5c2f7e75f4994d6a4f55a138e
Yongveloper <aaa9015@naver.com>
2021-04-11 18:53:27 +0900

    theme 파일 생성 후 분리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,10 @@ import React from 'react';
 import Router from './Routers';
 import GlobalStyles from './Components/Common/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 function App() {
   return (
-    <ThemeProvider
-      theme={{
-        palette: {
-          white: '#ffffff',
-          darkblue: '#00112d',
-          gray: '#2b3137',
-          lightGray: '#a0a0a0',
-          black: '#000000',
-          orange: '#fd6f22',
-          red: '#f44336',
-          jade: '#4D9BB0',
-          lightJade: '#EFF7F9',
-          purple: '#a06cff',
-        },
-      }}
-    >
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Router />
     </ThemeProvider>
