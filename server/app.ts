f56d634bd530f4b7aa929298f76a4631a1072d15
maru5mango <mjel0507@gmail.com>
2021-04-15 10:19:35 +0900

    [Setting] MongoDB Connected ..

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 
 const app = express();
-const port = 5000;
+
+
+const config = require("./config/key");
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI,
+    {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+    }).then(()=>console.log('MongoDB Connected..'))
+      .catch((err:any) => console.log(err));
 
 app.get('/',(req:express.Request, res: express.Response)=>{
     res.send('hello typescript!');
 });
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server listnening on ${port}`);
 });
