7a8086e3d44c1bb5a08239f6a9ceaf6d3c30cdde
maru5mango <mjel0507@gmail.com>
2021-04-15 14:53:14 +0900

    [Setting] Proxy 연걸

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TestServer from './TestServer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TestServer />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+const cors=require('cors');
+app.use(cors());
 
+const fs=require('fs');
 
 const config = require("./config/key");
 const mongoose = require("mongoose");
@@ -15,6 +19,9 @@ const connect = mongoose.connect(config.mongoURI,
 app.get('/',(req:express.Request, res: express.Response)=>{
     res.send('hello typescript!');
 });
+
+const UserRouter = require('./routes/User');
+app.use('/users',UserRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
