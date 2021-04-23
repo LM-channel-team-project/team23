44bafcd968d1d76125afc285617c6777ef6d8494
Yongveloper <aaa9015@naver.com>
2021-04-24 00:26:42 +0900

    feat: 질문 컴포넌트 마크업+스타일링

--- client/src/Components/ProjectDetail/Question.tsx
+++ client/src/Components/ProjectDetail/Question.tsx
@@ -1,7 +1,88 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function Question() {
-  return <div>7777</div>;
-}
+const Container = styled.div``;
+
+const InputSection = styled.div`
+  width: 100%;
+  padding: 26px 40px 24px;
+  background-color: ${(props) => props.theme.palette.faintGray};
+  font-family: 'Noto Sans', sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
+`;
+
+const TabTitle = styled.h3`
+  font-size: 1.375rem;
+  line-height: 1.4375rem;
+  font-weight: 700;
+  padding-bottom: 15px;
+`;
+
+const InputWrap = styled.div`
+  display: flex;
+  margin-bottom: 12px;
+`;
+
+const Thumb = styled.div`
+  width: 44px;
+  height: 44px;
+  margin-right: 16px;
+  border-radius: 100px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const TextInput = styled.textarea`
+  width: 100%;
+  padding: 13px 16px;
+  border: 1px solid #c9c9d9;
+  font-size: 0.8rem;
+  border-radius: 4px;
+  resize: none;
+  font-family: 'Noto Sans', sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
+`;
+
+const ButtonWrapper = styled.div`
+  width: calc(100% - 60px);
+  margin-left: 60px;
+`;
+
+const Button = styled.button`
+  background-color: #42495b;
+  color: #fff;
+  border: 1px solid #42495b;
+  width: auto;
+  padding: 8px 29px !important;
+  border-radius: 4px;
+  font-family: 'Noto Sans', sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
+`;
+
+const Question = () => {
+  return (
+    <Container>
+      <InputSection>
+        <TabTitle>응원/질문을 올려주세요!</TabTitle>
+        <InputWrap>
+          <Thumb>
+            <Image src="https://letspl.me/assets/images/prof_noImg.svg" />
+          </Thumb>
+          <TextInput
+            placeholder="프로젝트에 관한 질문이나 응원메시지를 올려주세요"
+            maxLength={1000}
+            rows={6}
+          />
+        </InputWrap>
+        <ButtonWrapper>
+          <Button>등록</Button>
+        </ButtonWrapper>
+      </InputSection>
+    </Container>
+  );
+};
 
 export default Question;
