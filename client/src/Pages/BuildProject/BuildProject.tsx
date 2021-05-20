67e77528ac0757290ba07dcd9dde99b4e6a6d26c
Yongveloper <aaa9015@naver.com>
2021-05-21 00:07:28 +0900

    feat: sampleImages 파일로 분리함

--- client/src/Pages/BuildProject/BuildProject.tsx
+++ client/src/Pages/BuildProject/BuildProject.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { FaCamera } from 'react-icons/fa';
+import { sampleImages } from '../../Components/BuildProject/sampleImages';
 import Button from '../../Components/Common/Button';
 import SelectBox from '../../Components/Common/SelectBox';
 import InputBox from '../../Components/Common/InputBox';
@@ -156,39 +157,6 @@ const CountWrapper = styled.div`
 const TwoBtnWrapper = styled.div``;
 const DateWrapper = styled.div``;
 const RefWrapper = styled.div``;
-
-const sampleImages = [
-  {
-    url:
-      'https://letspl.s3.ap-northeast-2.amazonaws.com/images/projectThumb_1.png',
-    alt: 'sample1',
-  },
-  {
-    url:
-      'https://letspl.s3.ap-northeast-2.amazonaws.com/images/projectThumb_2.png',
-    alt: 'sample2',
-  },
-  {
-    url:
-      'https://letspl.s3.ap-northeast-2.amazonaws.com/images/projectThumb_3.png',
-    alt: 'sample3',
-  },
-  {
-    url:
-      'https://letspl.s3.ap-northeast-2.amazonaws.com/images/projectThumb_4.png',
-    alt: 'sample4',
-  },
-  {
-    url:
-      'https://letspl.s3.ap-northeast-2.amazonaws.com/images/projectThumb_5.png',
-    alt: 'sample5',
-  },
-  {
-    url:
-      'https://letspl.s3.ap-northeast-2.amazonaws.com/images/projectThumb_6.png',
-    alt: 'sample6',
-  },
-];
 
 function BuildProject() {
   const [projectTitle, setProjectTitle] = useState('');
