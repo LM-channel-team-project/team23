78ef878015e013a1dbfc4ba19618430d0913c482
dhgu-dev <82129110+dhgu-dev@users.noreply.github.com>
2021-06-11 21:16:12 +0900

    Update BuildProject.tsx
    writer 중복 제거

--- client/src/Pages/BuildProject/BuildProject.tsx
+++ client/src/Pages/BuildProject/BuildProject.tsx
@@ -239,7 +239,6 @@ function BuildProject() {
         startAt: startDate,
         endAt: endDate,
         projectLV: level,
-        writer: userId,
         receivedLike: 0,
       };
       const { data } = await axios.post(
