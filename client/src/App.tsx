b4d3ee67e75f3b57d2a321f83a5d0b95cfdf296e
Yongveloper <aaa9015@naver.com>
2021-06-21 21:57:59 +0900

    feat:  Add  redux-devtools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,12 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './modules';
 import Thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, applyMiddleware(logger, Thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, Thunk))
+);
 
 function App() {
   return (
